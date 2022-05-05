82f1167319f335c2053cfdec9fb6d8ce765518e0
yemreeke <yemreeke1@gmail.com>
2022-05-05 20:45:01 +0300

    Proje Oluşturuldu

---
+++ Swift-UI-1/Swift-UI-1/Swift_UI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_1App.swift
+//  Swift-UI-1
+//
+//  Created by Emre on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

750e018cd281d717e66d7a402c04d861e9bcbd38
yemreeke <yemreeke1@gmail.com>
2022-05-05 20:52:58 +0300

    Tamamlandı.

--- Swift-UI-1/Swift-UI-1/ContentView.swift
+++ Swift-UI-1/Swift-UI-1/ContentView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isTurkish : Bool = true
     var body: some View {
         VStack{
-            Text("Hello, world!")
-        }
+            if(isTurkish){
+                Text("Merhaba, dünya!")
+            }
+            else{
+                Text("Hello, world!")
+            }
+            Button(action: {
+                isTurkish.toggle()
+            }, label: {
+                Text(isTurkish ? "Hello" : "Merhaba")
+            })
+        }.font(.title)
     }
 }
 

5e3704f2601b790fc6f6e33e8b3d2fc830b3e205
yemreeke <yemreeke1@gmail.com>
2022-05-09 18:18:32 +0300

    Swift-UI-2 Oluşturuldu.

---
+++ Swift-UI-2/Swift-UI-2/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Swift-UI-2
+//
+//  Created by Emre on 9.05.2022.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTurkish : Bool = true
+    var body: some View {
+        VStack{
+            if(isTurkish){
+                Text("Merhaba, dünya!")
+            }
+            else{
+                Text("Hello, world!")
+            }
+            Button(action: {
+                isTurkish.toggle()
+            }, label: {
+                Text(isTurkish ? "Hello" : "Merhaba")
+            })
+        }.font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-UI-2/Swift-UI-2/Swift_UI_2App.swift
@@ -0,0 +1,21 @@
+//
+//  Swift_UI_2App.swift
+//  Swift-UI-2
+//
+//  Created by Emre on 9.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+/*
+ Uygulama İsmi ->Targets->Swift-UI-2->Identity->Display Name
+ */

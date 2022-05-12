0ac4ee458e6cddda7130099d603716925dcbcabd
yemreeke <yemreeke1@gmail.com>
2022-05-12 10:26:42 +0300

    Swift-UI-3 Tamalandı.

--- Swift-UI-3.playground/Contents.swift
+++ Swift-UI-3.playground/Contents.swift
@@ -22,7 +22,7 @@ let number = 51
 let fmStation = 91.2
 
 let countingUp = ["one","two"]
-let nameByParkingSpace = [13:"Ahmet",15:"Mehmet"]
+var nameByParkingSpace = [13:"Ahmet",15:"Mehmet"]
 
 countingUp[0]
 
@@ -77,3 +77,90 @@ else{
 }
 
 nameByParkingSpace[13]
+nameByParkingSpace[14] //Program çökmüyor nil döndürüyor.
+
+// countingUp[3] //Program Çöküyor
+
+for i in 0..<5{
+    print(i) // 0 1 2 3 4
+}
+for i in 0...5{
+    print(i) // 0 1 2 3 4 5
+}
+
+for string in countingUp{
+    print(string) //one two
+}
+for (index,string) in countingUp.enumerated(){ //string dizisi
+    print("\(index) : \(string)") //0 one 1 two
+}
+for (key,value) in nameByParkingSpace{ //sözlük
+    print("\(key) : \(value)")
+    //15 : Mehmet
+    //13 : Ahmet
+}
+nameByParkingSpace[14]="Emre"
+for (key,value) in nameByParkingSpace{ //sözlük
+    print("\(key) : \(value)")
+    //15 : Mehmet
+    //14 : Emre
+    //13 : Ahmet
+}
+
+enum PieType{ //Turta Tipi
+    case apple //elma
+    case apricot //kayısı
+    case peach //şeftali
+}
+let favoritePie = PieType.apricot
+print(favoritePie)
+
+let name:String
+//name = "Emre" //1.de atar
+//name = "Emre" //2. de hata verir
+
+switch favoritePie{
+case .apple:
+    name = "Apple Pie"
+case .apricot:
+    name = "Apricot Pie"
+case .peach:
+    name = "Peach Pie"
+}
+print(name)
+
+let macOsVersion : Int = 12
+switch macOsVersion{
+case 0...8:
+    print("A big cat")
+case 9:
+    print("Mavericks")
+case 10:
+    print("Yosemite")
+default:
+    print("Wow, new Mac Version")
+}
+
+enum PieType2: String{
+    case apple = "Apple Pie"
+    case apricot = "Apricot Pie"
+    case peach = "Peach Pie"
+}
+let favoritePie2 = PieType2.apple
+favoritePie2.rawValue //Apple Pie
+
+// Ortak özellikler
+// İçlerine değerler alabiliyorlar
+// Kendi fonksiyonları da olabiliyor.
+// Class ve Struct Fark
+// Class Referance type
+// Hafıza yer tutuyor değiştiriliyor.
+// Struct Değer tipi
+// Hafızada Kopyalanıyor.
+class ClassName{
+    
+}
+
+struct StructName{
+    
+}

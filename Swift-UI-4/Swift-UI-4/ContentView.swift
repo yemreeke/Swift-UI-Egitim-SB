0561bca509f7bf37c83602568970a8031e937b1a
yemreeke <yemreeke1@gmail.com>
2022-05-16 18:39:39 +0300

    Swift-UI-4 Labellar yazıldı.

---
+++ Swift-UI-4/Swift-UI-4/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Swift-UI-4
+//
+//  Created by Emre on 16.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("212")
+            Text("Fahrenheit")
+            Text("100")
+            Text("degress Celcius")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-UI-4/Swift-UI-4/Swift_UI_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_4App.swift
+//  Swift-UI-4
+//
+//  Created by Emre on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

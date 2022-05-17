0aadb0b8b7e4b41d9ea204f813c2c7ce4418eb4f
yemreeke <yemreeke1@gmail.com>
2022-05-17 17:19:14 +0300

    Swift-UI-5 Başlandı.

---
+++ Swift-UI-5/Swift-UI-5/Swift_UI_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_5App.swift
+//  Swift-UI-5
+//
+//  Created by Emre on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
